75f571546a37e6c970cf93f051669cbb2ba0b600
jhblue97 <jhblue97@naver.com>
2019-01-31 12:17:26 +0900

    01-31 주현 : 커밋처음

--- app/src/main/java/example/zaglezagle/MainActivity.java
+++ app/src/main/java/example/zaglezagle/MainActivity.java
@@ -6,8 +6,16 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public boolean onSupportNavigateUp() {
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
